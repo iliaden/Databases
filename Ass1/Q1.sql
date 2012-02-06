e31e9bc25fdb932119161ba54d1c4e463727dc63
Ilia Denotkine <denotkine@gmail.com>
2012-02-06 14:26:30 -0500

    A1 done

---
+++ Ass1/Q1.sql
@@ -0,0 +1,89 @@
+add table Participants
+(
+    INTEGER unique_key NOT NULL AUTOINCRMENT,
+    VARCHAR(20) first_name NOT NULL,
+    VARCHAR(20) last_name NOT NULL,
+    INTEGER address NOT NULL,
+    FOREIGN KEY address REFERENCES Address(unique_key),
+    PRIMARY KEY (unique_key)
+);
+
+add table Trainers
+(
+    INTEGER unique_key NOT NULL AUTOINCRMENT,
+    VARCHAR(20) first_name NOT NULL,
+    VARCHAR(20) last_name NOT NULL,
+    INTEGER hourly_wage NOT NULL,
+    INTEGER address NOT NULL,
+    FOREIGN KEY address REFERENCES Address(unique_key),
+    PRIMARY KEY (unique_key)
+);
+
+add table Address
+(
+    INTEGER unique_key NOT NULL AUTOINCREMENT,
+    VARCHAR(20) street_name NOT NULL,
+    VARCHAR(10) street_number NOT NULL, #so 12A can work
+    VARCHAR(10) appartment,
+    VARCHAR(6) postal_code NOT NULL,
+    VARCHAR(20) province NOT NULL,
+    VARCHAR(20) country NOT NULL,  
+    PRIMARY KEY (unique_key),
+    UNIQUE(street_name,street_number, appartment, postal_code, country) #sufficient uniqueness conditions
+
+);
+
+add table Program
+(
+    INTEGER unique_key NOT NULL AUTOINCREMENT,
+    VARCHAR(20) name NOT NULL,
+    INTEGER max_participant_count NOT NULL,
+    INTEGER cost NOT NULL,
+    PRIMARY KEY (unique_key)
+);
+
+add table Program_trainers
+(
+    INTEGER trainer NOT NULL,
+    INTEGER program NOT NULL,
+    PRIMARY KEY (trainer, program),
+    FOREIGN KEY trainer REFERENCES Trainers(unique_key),
+    FOREIGN KEY program REFERENCES Programs(unique_key)
+);
+
+add table Program_participation
+(
+    INTEGER participant NOT NULL,
+    INTEGER program NOT NULL,
+    PRIMARY KEY (participant, program),
+    FOREIGN KEY participant REFERENCES Participants(unique_key),
+    FOREIGN KEY program REFERENCES Programs(unique_key)
+);
+
+add table Documents
+(
+    INTEGER unique_key NOT NULL AUTOINCREMENT,
+    VARCHAR(500) description NOT NULL,
+    INTEGER previous_revision,
+    FOREIGN KEY previous_revision REFERENCES Documents(unique_key),
+    PRIMARY KEY (unique_key)
+);
+
+add table Document_authors
+(
+    INTEGER document NOT NULL,
+    INTEGER author NOT NULL,
+    PRIMARY KEY (document, author),
+    FOREIGN KEY document REFERENCES Documents(qunie_key),
+    FOREIGN KEY author REFERENCES Authors(unique_key)
+);
+
+add table Program_documents
+(
+    INTEGER program NOT NULL,
+    INTEGER document NOT NULL,
+    PRIMARY KEY (program, document),
+    FOREIGN KEY document REFERENCES Documents(qunie_key),
+    FOREIGN KEY program REFERENCES Programs(unique_key)
+);
+
